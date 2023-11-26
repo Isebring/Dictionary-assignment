dc0cc6128807e859bcdddcdd86a6652fe8db718e
Hampus Isebring <hampus.isebring@gmail.com>
2023-11-26 19:49:44 +0100

    Created card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,87 @@
+import { IconVolume } from "@tabler/icons-react";
+import React, { useEffect, useRef } from "react";
+import { Word } from "../pages/Landingpage";
+
+interface CardProps {
+  word: Word;
+  activeAudio: string | null;
+  playAudio: () => void;
+}
+
+const Card: React.FC<CardProps> = ({ word, activeAudio }) => {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  useEffect(() => {
+    if (audioRef.current && activeAudio) {
+      audioRef.current.src = activeAudio;
+      audioRef.current.load();
+    }
+  }, [activeAudio]);
+
+  const playAudio = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
+  return (
+    <div>
+      <h4>Word:</h4>
+      <p>{word.word}</p>
+      {word.phonetics && word.phonetics[0] && (
+        <div>
+          <h4>Phonetics:</h4>
+          <p>Text: {word.phonetics[0].text}</p>
+          {activeAudio && (
+            <div>
+              <p>Audio:</p>
+              <audio ref={audioRef} />
+              <source src={activeAudio} type="audio/mpeg" />
+              <IconVolume style={{ cursor: "pointer" }} onClick={playAudio} />
+            </div>
+          )}
+        </div>
+      )}
+      {word.origin && (
+        <div>
+          <h4>Origin:</h4>
+          <p>{word.origin}</p>
+        </div>
+      )}
+      {word.meanings && word.meanings[0] && (
+        <div>
+          <h4>Part of Speech:</h4>
+          <p>{word.meanings[0].partOfSpeech}</p>
+          {word.meanings[0].definitions && word.meanings[0].definitions[0] && (
+            <div>
+              <h4>Definition:</h4>
+              <p>{word.meanings[0].definitions[0].definition}</p>
+              {word.meanings[0].definitions[0].example && (
+                <div>
+                  <h4>Example:</h4>
+                  <p>{word.meanings[0].definitions[0].example}</p>
+                </div>
+              )}
+              {word.meanings[0].definitions[0].synonyms &&
+                word.meanings[0].definitions[0].synonyms.length > 0 && (
+                  <div>
+                    <h4>Synonyms:</h4>
+                    <p>{word.meanings[0].definitions[0].synonyms.join(", ")}</p>
+                  </div>
+                )}
+              {word.meanings[0].definitions[0].antonyms &&
+                word.meanings[0].definitions[0].antonyms.length > 0 && (
+                  <div>
+                    <h4>Antonyms:</h4>
+                    <p>{word.meanings[0].definitions[0].antonyms.join(", ")}</p>
+                  </div>
+                )}
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/Landingpage.tsx
+++ src/pages/Landingpage.tsx
@@ -1,10 +1,10 @@
-import { IconVolume } from "@tabler/icons-react";
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
+import Card from "../components/Card";
+import "./Landingpage.css";
 
-interface Word {
+export interface Word {
   word: string;
-  // phonetic: string;
   phonetics: Array<{
     text: string;
     audio?: string;
@@ -62,100 +62,28 @@ function Landingpage() {
   }, [searchedWord]);
 
   const playAudio = () => {
-    if (audioRef.current && activeAudio) {
-      audioRef.current.src = activeAudio;
-      audioRef.current.load();
+    if (audioRef.current) {
       audioRef.current.play();
     }
   };
 
   return (
-    <div>
+    <div className="card">
       <input
+        className="input-search"
         type="search"
         placeholder="Search for a word.."
         value={selectedWord}
         onChange={(e) => setSelectedWord(e.target.value)}
       />
       <button onClick={handleSelect}>Search</button>
-      <div>
-        {searchedWord && (
-          <div>
-            <h4>Word:</h4>
-            <p>{searchedWord.word}</p>
-            {searchedWord.phonetics && searchedWord.phonetics[0] && (
-              <div>
-                <h4>Phonetics:</h4>
-                <p>Text: {searchedWord.phonetics[0].text}</p>
-                {activeAudio && (
-                  <div>
-                    <p>Audio:</p>
-                    <audio ref={audioRef} />
-                    <source src={activeAudio} type="audio/mpeg" />
-                    <IconVolume
-                      style={{ cursor: "pointer" }}
-                      onClick={playAudio}
-                    />
-                  </div>
-                )}
-              </div>
-            )}
-            {searchedWord.origin && (
-              <div>
-                <h4>Origin:</h4>
-                <p>{searchedWord.origin}</p>
-              </div>
-            )}
-            {searchedWord.meanings && searchedWord.meanings[0] && (
-              <div>
-                <h4>Part of Speech:</h4>
-                <p>{searchedWord.meanings[0].partOfSpeech}</p>
-                {searchedWord.meanings[0].definitions &&
-                  searchedWord.meanings[0].definitions[0] && (
-                    <div>
-                      <h4>Definition:</h4>
-                      <p>
-                        {searchedWord.meanings[0].definitions[0].definition}
-                      </p>
-                      {searchedWord.meanings[0].definitions[0].example && (
-                        <div>
-                          <h4>Example:</h4>
-                          <p>
-                            {searchedWord.meanings[0].definitions[0].example}
-                          </p>
-                        </div>
-                      )}
-                      {searchedWord.meanings[0].definitions[0].synonyms &&
-                        searchedWord.meanings[0].definitions[0].synonyms
-                          .length > 0 && (
-                          <div>
-                            <h4>Synonyms:</h4>
-                            <p>
-                              {searchedWord.meanings[0].definitions[0].synonyms.join(
-                                ", "
-                              )}
-                            </p>
-                          </div>
-                        )}
-                      {searchedWord.meanings[0].definitions[0].antonyms &&
-                        searchedWord.meanings[0].definitions[0].antonyms
-                          .length > 0 && (
-                          <div>
-                            <h4>Antonyms:</h4>
-                            <p>
-                              {searchedWord.meanings[0].definitions[0].antonyms.join(
-                                ", "
-                              )}
-                            </p>
-                          </div>
-                        )}
-                    </div>
-                  )}
-              </div>
-            )}
-          </div>
-        )}
-      </div>
+      {searchedWord && (
+        <Card
+          word={searchedWord}
+          activeAudio={activeAudio}
+          playAudio={playAudio}
+        />
+      )}
     </div>
   );
 }
